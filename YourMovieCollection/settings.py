81a0f058558d508ff5e281e01562564049e99e3b
Radhe <mkradhecs@gmail.com>
2022-12-12 20:22:05 +0530

    updated setting file

--- YourMovieCollection/settings.py
+++ YourMovieCollection/settings.py
@@ -1,6 +1,8 @@
 from datetime import timedelta
 from pathlib import Path
 from decouple import config
+import os
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,12 +12,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
